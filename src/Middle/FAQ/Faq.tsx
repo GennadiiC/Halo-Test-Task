c215571130ce92fc2106c105cedc4cbd5a9ab2b0
Gennadii Chamorsov <gchamorsov@gmail.com>
2023-02-16 12:23:22 +0200

    5th

--- src/Middle/FAQ/Faq.tsx
+++ src/Middle/FAQ/Faq.tsx
@@ -4,38 +4,6 @@ import { useContext } from 'react'
 
 import classes from './Faq.module.scss'
 
-// type question = {
-//   question: string
-//   answer: string
-//   id: number
-// }
-
-// const faq: question[] = [
-//   {
-//     question: 'What can I do to protect our planet?',
-//     answer: `Not to make an open fire in nature and to clean up litter; 
-//     not to pollute open water bodies; to switch to alternative 
-//     energy sources; to reduce the use of non-renewable 
-//     resources`,
-//     id: 1
-//   },
-//   {
-//     question: `How to save nature ecology?`,
-//     answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-//     Vivamus nec ultrices lacus, sit amet rhoncus est. 
-//     Donec pretium semper elit, id iaculis mi dapibus eget. 
-//     Praesent ut ante eu ante mattis varius. Nulla facilisi.`,
-//     id: 2
-//   },
-//   {
-//     question: `What is nature conservation?`,
-//     answer: `Vivamus eu feugiat velit, quis porttitor arcu. 
-//     Aenean sed pellentesque ipsum. In tempor tempor est, 
-//     aliquam bibendum enim aliquam nec. Proin vitae tempor augue.`,
-//     id: 3
-//   }
-// ]
-
 export default function Faq() {
 
   const { qa } = useContext(FaqContext)
